cc27106a2ad4db903599dd9de9ccd68b83032873
Nataliya-grish <gray-wings@yandex.ru>
2024-02-25 10:50:42 +0300

    start commit

---
+++ src/users/schema/users.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserModel = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ unique: true, required: true })
+  passwordHash: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  contactPhone: string;
+
+  @Prop({
+    required: true,
+    default: 'client',
+    role: 'client' || 'admin' || 'manager',
+  })
+  static role: string;
+
+  @Prop({ required: true })
+  id: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
